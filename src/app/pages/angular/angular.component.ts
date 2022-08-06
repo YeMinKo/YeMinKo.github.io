ea76466467cfd650a325b20de0211445d586e91f
Ye Min Ko <yeminko47@gmail.com>
2022-08-06 15:00:30 +0630

    [added] project links

--- src/app/pages/angular/angular.component.ts
+++ src/app/pages/angular/angular.component.ts
@@ -18,51 +18,51 @@ export class AngularComponent implements OnInit {
       poster: 'calculator.png',
       title: 'Calculator App',
       description: 'Simple calculator app built with Angular.',
-      link: 'https://yeminko.github.io/note-app/',
+      link: 'https://yeminko.github.io/calculator-app/',
     },
     {
       poster: 'todo.png',
       title: 'Todo App',
       description: 'Simple todo app built with Angular.',
-      link: '',
+      link: 'https://yeminko.github.io/todo-app/',
     },
     {
       poster: 'note.png',
       title: 'Note App',
       description: 'Simple note taking app built with Angular.',
-      link: '',
+      link: 'https://yeminko.github.io/note-app/',
     },
     {
       poster: 'minipos.png',
       title: 'Mini POS',
       description: 'Simple mini POS built with Angular.',
-      link: '',
+      link: 'https://yeminko.github.io/mini-pos/',
     },
     {
       poster: 'text-finder.png',
-      title: 'Calculator App',
+      title: 'Text Finder',
       description: 'Simple text finder app that built with Angular directive.',
-      link: '',
+      link: 'https://yeminko.github.io/text-finder/',
     },
     {
       poster: 'flashcard-app.png',
       title: 'Flashcard App',
       description: 'Reactive Flashcard App that built with Angular.',
-      link: '',
+      link: 'https://yeminko.github.io/flashcard-app/',
     },
     {
       poster: 'calendar.png',
       title: 'Calendar App',
       description:
         'Simple calendar app that can manage events. Built with Angular and angular-calendar library.',
-      link: '',
+      link: 'https://yeminko.github.io/calendar-app/',
     },
     {
       poster: 'password-manager.png',
       title: 'Password Manager',
       description:
         'Mini password manager app built with Angular. All user data will encrypt with AES 256 (crypto-js).',
-      link: '',
+      link: 'https://ng-password-manager.web.app/',
     },
   ];
 
